3bb3cfc4b492271b5dbd35ef58afc19a69d10c76
Faisal-althuwaini <faisal.yalthuwaini@gmail.com>
2025-01-20 09:22:16 +0300

    Adding Kafka functionality to updateBook

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/segmentio/kafka-go"
 	"log"
 
 	pb "faisal.com/bookProject/server/proto"
@@ -25,6 +26,13 @@ func main() {
 	}
 
 	defer conn.Close()
+
+	// Create Kafka Writer for producing
+	kafkaWriter := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{"localhost:9092"}, // Kafka broker
+		Topic:   "book-events",              // Kafka topic for updating books
+	})
+	defer kafkaWriter.Close()
 
 	client := pb.NewBookServiceClient(conn)
 
@@ -81,6 +89,37 @@ func main() {
 		}
 
 		return c.JSON(resp.Message)
+	})
+
+	app.Put("/book", func(c *fiber.Ctx) error {
+		// Parse input
+		var data struct {
+			Id       string `json:"id"`
+			BookName string `json:"book_name"`
+		}
+
+		if err := c.BodyParser(&data); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Invalid request",
+			})
+		}
+
+		// Produce update message to kafka
+		message := kafka.Message{
+			Key:   []byte(data.Id),
+			Value: []byte(data.BookName),
+		}
+
+		if err := kafkaWriter.WriteMessages(context.Background(), message); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Failed to send update message to Kafka",
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"message": "Book updated successfully",
+		})
+
 	})
 
 	log.Println("Fiber server is running on http://localhost:3000")
